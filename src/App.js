31163085de9fb7f794177682a9eeae160ff80ec1
dpaz2290 <75946140+dpaz2290@users.noreply.github.com>
2021-04-11 11:28:37 -0500

    Added the GitHuib repository URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Wetaher App</div>
+    <div className="App">
+      <h1>
+      Wetaher App
+      </h1>
+    <footer>
+      This project was coded by Matt Delac and is {" "}
+      <a href= "https://github.com/dpaz2290/react-weather-project" target="_blank">
+        open sourced on GitHub
+        </a>
+    </footer>
+    </div>
   );
 }
 
