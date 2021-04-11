e95f68a54072667693e783385c5c27ea2d1f256f
dpaz2290 <75946140+dpaz2290@users.noreply.github.com>
2021-04-11 13:10:27 -0500

    Basic structure and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-      Wetaher App
-      </h1>
+      <div className="container">
+      <Weather />
     <footer>
       This project was coded by Matt Delac and is {" "}
       <a href= "https://github.com/dpaz2290/react-weather-project" target="_blank">
         open sourced on GitHub
         </a>
     </footer>
+    </div>
     </div>
   );
 }
