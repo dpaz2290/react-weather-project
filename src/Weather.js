941e57b140474319a283cc35505498b8d6b8fb9e
dpaz2290 <75946140+dpaz2290@users.noreply.github.com>
2021-04-11 23:54:56 -0500

    loading trouble

--- src/Weather.js
+++ src/Weather.js
@@ -39,19 +39,19 @@ export default function Weather (props) {
 
 if (weatherData.ready) {
     return <div className="Weather">
-        <form onSubmit={handleSubmit}>
-            <div className="row">
-                <div className="col-9">
-                <input type="search" placeholder= "Enter a city" className="form-control" autoFocus="on" onChange={handleCityChange} />
-                </div>
-                <div className="col-3">
-                <input type="submit" value="search" className="btn btn-primary w-100" />
-                </div>
-            </div>
-        </form>
-        <WeatherInfo data={weatherData}/>
+                <form onSubmit={handleSubmit}>
+                    <div className="row">
+                        <div className="col-9">
+                        <input type="search" placeholder= "Enter a city" className="form-control" autoFocus="on" onChange={handleCityChange} />
+                        </div>
+                        <div className="col-3">
+                        <input type="submit" value="search" className="btn btn-primary w-100" />
+                        </div>
+                    </div>
+                </form>
+                <WeatherInfo data={weatherData}/>
         
-        </div>
+             </div>
 } else {
     search ();
     return "The app is loading...";
